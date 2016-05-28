668da96d8e244abc05a217b3bb477589936e967a
Naveen Kumar <dnk484@gmail.com>
2016-05-28 14:14:00 -0500

    start

---
+++ Hello.py
@@ -0,0 +1,12 @@
+'''
+Created on May 28, 2016
+
+@author: t420
+'''
+from pip._vendor.distlib.compat import raw_input
+
+print Provide a integer input
+a= raw_input()
+print("Hello World")
+print(a)
+
